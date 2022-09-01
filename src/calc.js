bb6902df1397861c98c3b52878aa49a1c4fae067
zhig1ov <zhiglov.artem@yandex.ru>
2022-09-01 16:19:15 +0500

    refactor: function refactoring

--- src/calc.js
+++ src/calc.js
@@ -1,23 +1,17 @@
-import readlineSync from 'readline-sync';
+import { incorrectResponse, getAnswer } from './utils/utils.js';
+import sayHi from './cli.js';
+
+const operators = ['+', '-', '*'];
+const expressions = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
 
 const brainCalc = () => {
-  function incorrectResponse(youAnswer, userName, correctAnswer) {
-    console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-  }
-
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  const userName = sayHi();
 
   console.log('What is the result of the expression?');
-
-  const operators = ['+', '-', '*'];
-  const expressions = {
-    '+': (a, b) => a + b,
-    '-': (a, b) => a - b,
-    '*': (a, b) => a * b,
-  };
 
   for (let i = 1; i <= 3; i += 1) {
     const randNum1 = Math.floor(Math.random() * 99) + 1;
@@ -26,9 +20,7 @@ const brainCalc = () => {
     const expression = `${randNum1} ${operator} ${randNum2}`;
     const correctAnswer = expressions[operator](randNum1, randNum2);
 
-    const question = `Question: ${expression}`;
-    console.log(question);
-    const youAnswer = readlineSync.question('Your answer: ');
+    const youAnswer = getAnswer(expression);
 
     if (parseInt(youAnswer, 10) === correctAnswer) {
       console.log('Correct!');
@@ -36,10 +28,7 @@ const brainCalc = () => {
       incorrectResponse(youAnswer, userName, correctAnswer);
       break;
     }
-
-    if (i === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    }
+    if (i === 3) { console.log(`Congratulations, ${userName}!`); }
   }
 };
 
--- src/even.js
+++ src/even.js
@@ -1,24 +1,16 @@
-import readlineSync from 'readline-sync';
-// import sayHi from './cli.js';
+import { incorrectResponse, getAnswer } from './utils/utils.js';
+import sayHi from './cli.js';
 
 const brainEven = () => {
-  function incorrectResponse(youAnswer, userName, correctAnswer) {
-    console.log(`'${youAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-  }
-
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  const userName = sayHi();
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   for (let i = 1; i <= 3; i += 1) {
     const random = Math.floor(Math.random() * 99) + 1;
 
-    const question = `Question: ${random}`;
-    console.log(question);
-    const youAnswer = readlineSync.question('Your answer: ');
+    const youAnswer = getAnswer(random);
+
     if (random % 2 === 0) {
       if (youAnswer === 'yes') {
         console.log('Correct!');
@@ -34,9 +26,7 @@ const brainEven = () => {
         break;
       }
     }
-    if (i === 3) {
-      console.log(`Congratulations, ${userName}!`);
-    }
+    if (i === 3) { console.log(`Congratulations, ${userName}!`); }
   }
 };
 
